42f6c015793a1762691e0fa81a3fdf52bf600b35
jeandavidt <jeandavidt@gmail.com>
2019-12-16 17:22:41 -0500

    Metrics are updated and sample graph works

--- app.py
+++ app.py
@@ -1,56 +1,129 @@
+from datetime import datetime, timedelta, timezone
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
+import pandas as pd
+import plotly.io as pio
 
 import Dateaubase
 import PlottingTools
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+pio.templates.default = "plotly_white"
+
+NEW_DATA_INTERVAL = 30  # in seconds
+DAYS_OF_DATA = 1
+STORE_MAX_LENGTH = DAYS_OF_DATA * 24 * 60 * 60  # worst-case of sensor that updates every second
+
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-cursor, conn = Dateaubase.create_connection()
 
-app.layout = html.Div(children=[
-    html.H1(children=dcc.Markdown('pil*EAU*te Dashboard')),
-    dcc.Interval(id='refresh_interval', interval=60 * 1000, n_intervals=0),
-    html.Div(children='''
-        Graph example (tries to update every 60 seconds)
-    '''),
-
-    dcc.Graph(id='example_graph')
-])
+app.layout = html.Div(
+    children=[
+        dcc.Store(id='avn-db-store'),
+        html.H1(children=dcc.Markdown('pil*EAU*te Dashboard')),
+        dcc.Interval(id='refresh-interval', interval=NEW_DATA_INTERVAL * 1000, n_intervals=0),
+        html.Div(
+            children='''Graph example (tries to update every 60 seconds'''
+        ),
+        dcc.Graph(id='example-graph', animate=True)
+    ]
+)
 
 
 @app.callback(
-    Output('example_graph', 'figure'),
-    [Input('refresh_interval', 'n_intervals')])
-def draw_example_graph(n):
+    Output('avn-db-store', 'data'),
+    [Input('refresh-interval', 'n_intervals')],
+    [State('avn-db-store', 'data')])
+def store_data(n, data):
     try:
         _, conn = Dateaubase.create_connection()
     except Exception:
         raise PreventUpdate
-    Start = Dateaubase.date_to_epoch('2017-09-01 12:00:00')
-    End = Dateaubase.date_to_epoch('2017-10-01 12:00:00')
-    Location = 'Primary settling tank effluent'
+    print('level 0')
+    end = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
+    three_days_ago = datetime.now(timezone.utc) - timedelta(days=DAYS_OF_DATA)
+    if not data:
+        print('level 1')
+        start = datetime.strftime(three_days_ago, '%Y-%m-%d %H:%M:%S')
+    else:
+        print('level 2')
+        stored_df = pd.read_json(data)
+        print('what is the latest time?')
+        print(pd.to_datetime(list(stored_df.index)[-1]))
+        last_time = pd.to_datetime(list(stored_df.index)[-1])
+        print(f'the type is {type(last_time)}')
+        
+        if last_time < three_days_ago:
+            print('level 3')
+            start = datetime.strftime(three_days_ago, '%Y-%m-%d %H:%M:%S')
+        else:
+            print('level 4')
+            start = datetime.strftime(last_time, '%Y-%m-%d %H:%M:%S')
+    print('level 0b')
     Project = 'pilEAUte'
-    param_list = ['COD', 'CODf', 'NH4-N', 'K']
-    equip_list = ['Spectro_010', 'Spectro_010', 'Ammo_005', 'Ammo_005']
-
+    Location = [
+        'Primary settling tank effluent',
+        'Pilote effluent',
+        'Pilote effluent',
+        'Pilote reactor 5'
+    ]
+    equip_list = [
+        'Ammo_005',
+        'Varion_002',
+        'Varion_002',
+        'FIT-430'
+    ]
+    param_list = [
+        'NH4-N',
+        'NH4-N',
+        'NO3-N',
+        'Flowrate (Gas)',
+    ]
     extract_list = {}
     for i in range(len(param_list)):
         extract_list[i] = {
-            'Start': Start,
-            'End': End,
+            'Start': Dateaubase.date_to_epoch(start),
+            'End': Dateaubase.date_to_epoch(end),
             'Project': Project,
-            'Location': Location,
+            'Location': Location[i],
             'Parameter': param_list[i],
             'Equipment': equip_list[i]
         }
-    df = Dateaubase.extract_data(conn, extract_list)
-    fig = PlottingTools.extract_plotly(df)
-    return fig
+
+    new_df = Dateaubase.extract_data(conn, extract_list)
+    length_new = len(new_df)
+    if not data:
+        print('level 5')
+        complete_df = new_df
+    elif length_new == 0:
+        print('level 6')
+        raise PreventUpdate
+    else:
+        print('level 7')
+        # print(f'trying to concat dataframes. New df has {length_new} lines')
+        complete_df = pd.concat([stored_df, new_df], sort=True)
+        complete_df = complete_df.iloc[length_new:]
+    print('level 0c')
+    json_data = complete_df.to_json(date_format='iso')
+    return json_data
+
+
+@app.callback(
+    Output('example-graph', 'figure'),
+    [Input('avn-db-store', 'data')]
+)
+def example_graph(data):
+    if not data:
+        raise PreventUpdate
+    else:
+        df = pd.read_json(data)
+        fig = PlottingTools.extract_plotly(df)
+        print('fig has been drawn')
+        return fig
 
 
 if __name__ == '__main__':
